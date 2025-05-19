ed49c6ea4ef35c460fbed1b49d0645aafc49f4af
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-19 19:28:20 +0100

    Added test cases to check attribute presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, :last_name, :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new(name: "Bob", email: "example@example.com") }
+  describe "validation" do
+    let(:user) { User.new(first_name: "Bob", last_name: "Page", email: "example@example.com") }
+
+    it "must have a first name" do
+      user.first_name = nil
+      user.valid?
+
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "must have a last name" do
+      user.last_name = nil
+      user.valid?
+
+      expect(user.errors[:last_name]).to include("can't be blank")
+    end
+
+    it "must have an email" do
+      user.email = nil
+      user.valid?
+
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
 end
