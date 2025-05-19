ba7b38e50f64e8323f21b756176e1046aa3a6ec6
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-19 19:45:28 +0100

    Added length validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true
 end
