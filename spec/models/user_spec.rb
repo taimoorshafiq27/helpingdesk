7d7a029b8338cb97aba6dae9808c2aaf900666c1
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-19 19:45:16 +0100

    Added length test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,5 +24,19 @@ RSpec.describe User, type: :model do
 
       expect(user.errors[:email]).to include("can't be blank")
     end
+
+    it "must have a first name shorter than 30 characters" do
+      user.first_name = "a" * 31
+      user.valid?
+
+      expect(user.errors[:first_name]).to include("is too long (maximum is 30 characters)")
+    end
+
+    it "must have a last name shorter than 30 characters" do
+      user.last_name = "a" * 31
+      user.valid?
+
+      expect(user.errors[:last_name]).to include("is too long (maximum is 30 characters)")
+    end
   end
 end
