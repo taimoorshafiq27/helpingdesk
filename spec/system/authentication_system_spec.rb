935deeaca1cec93a32a413fafe1cef2ee86f27c6
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-21 13:26:20 +0100

    Added sign in system test

--- spec/system/authentication_system_spec.rb
+++ spec/system/authentication_system_spec.rb
@@ -11,7 +11,20 @@ RSpec.describe "Authentication", type: :system do
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_on "Sign up"
-
+    
     expect(page).to have_text("Welcome! You have signed up successfully.")
+  end
+  
+  it "must allow a user to sign in" do
+    user = create(:user)
+
+    visit user_session_url
+    assert_selector "h2", text: "Log in"
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_on "Log in"
+
+    expect(page).to have_text("Signed in successfully.")
   end
 end
