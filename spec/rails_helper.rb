0279bcd92ab29b2f7ff32de22190bccf9bc833f1
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-21 11:12:28 +0100

    Configured Capybara drivers for system tests in RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,4 +69,12 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   # 
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
 end
