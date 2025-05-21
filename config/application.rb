a2f9079aa6749680a05aaa223e2202da59f6ecbd
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-21 11:06:49 +0100

    Changed Rails generators to produce RSpec files

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Helpingdesk
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    # Generate RSpec test files instead of minitest files when a generator is run
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
