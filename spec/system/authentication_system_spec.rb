cb1342e0794d71496e0298d05d33e1dc734d65a5
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-21 12:48:18 +0100

    Added sign up test which currently fails

--- spec/system/authentication_system_spec.rb
+++ spec/system/authentication_system_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Authentication", type: :system do
+  it "must allow a user to sign up" do
+    visit new_user_registration_url
+    assert_selector "h2", text: "Sign up"
 
+    fill_in "Email", with: "example@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Sign up"
+
+    expect(page).to have_text("example@example.com")
+  end
 end
