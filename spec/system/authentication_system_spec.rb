1abaa3ef8498aeefdd6a8435d4de79d2e5fb3fb5
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-21 13:05:20 +0100

    Added Devise params and satisfied test with the params

--- spec/system/authentication_system_spec.rb
+++ spec/system/authentication_system_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe "Authentication", type: :system do
     visit new_user_registration_url
     assert_selector "h2", text: "Sign up"
 
+    fill_in "First name", with: "Bob"
+    fill_in "Last name", with: "Page"
     fill_in "Email", with: "example@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_on "Sign up"
 
-    expect(page).to have_text("example@example.com")
+    expect(page).to have_text("Welcome! You have signed up successfully.")
   end
 end
