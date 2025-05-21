e27c092dbd5af4974834604f63a66eceec1ba907
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-21 15:50:18 +0100

    Added roles controller + test boilerplate

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,52 @@
+class RolesController < ApplicationController
+  before_action :set_roles, only: [:index]
+  before_action :set_role, only: [:show, :edit, :update, :destroy]
+
+  def index; end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new(role_params)
+
+    if @role.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit; end
+
+  def update
+    if @role.update(role_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @role.destroy
+    redirect_to roles_path
+  end
+
+  private
+
+  def set_roles
+    @roles = Role.all
+  end
+
+  def set_role
+    @role = Role.find_by(id: params[:id])
+  end
+
+  def role_params
+    params.require(:role).permit(:name, :code)
+  end
+end
