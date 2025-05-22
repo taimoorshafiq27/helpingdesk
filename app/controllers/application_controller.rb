2080a75e93138a1728cd86732ec89bc8c50859fe
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-22 17:09:05 +0100

    Implementing menu and toolbar WIP with a bit of logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
