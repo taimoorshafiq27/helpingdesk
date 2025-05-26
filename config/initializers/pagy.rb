b93c81706ed4dbc898a2c3ba58e4b487a5b83375
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-26 11:36:37 +0100

    Added support for tailwind

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -4,3 +4,5 @@ Pagy::DEFAULT[:size]  = 9  # nav bar links
 # Better user experience handled automatically
 require 'pagy/extras/overflow'
 Pagy::DEFAULT[:overflow] = :last_page
+# Tailwind customisation
+require 'pagy/extras/tailwind'
