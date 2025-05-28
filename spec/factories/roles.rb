d16a912fbdba787f91fb6c0f563e543f64809677
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-28 19:14:40 +0100

    Added test case for mailer

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :role do
     name { "Administrator" }
     code { "ADM" }
+
+    trait :client do
+      name { "Client" }
+      code { "CLT" }
+    end
   end
 end
