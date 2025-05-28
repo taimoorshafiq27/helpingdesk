2f5eb06f5a235281661e903084ccb3a3b56de69c
taimoorshafiq27 <mtshafiq@outlook.com>
2025-05-28 20:47:54 +0100

    Added test coverage for role

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,6 @@ class Role < ApplicationRecord
   has_many :user_roles
   has_many :users, through: :user_roles
 
-  validates :name, presence: true
-  validates :code, presence: true, length: { is: 3 }
+  validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, length: { is: 3 }, uniqueness: true
 end
